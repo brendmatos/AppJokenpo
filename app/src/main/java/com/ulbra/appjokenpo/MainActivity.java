5075c651121f183851f42bb368e0cc10fe32b213
brendmatos <brendanselmom@hotmail.com>
2026-05-13 22:24:16 -0300

    Botão reiniciar jogada

--- app/src/main/java/com/ulbra/appjokenpo/MainActivity.java
+++ app/src/main/java/com/ulbra/appjokenpo/MainActivity.java
@@ -1,68 +1,103 @@
 package com.ulbra.appjokenpo;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import java.util.Random;
 
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnReiniciar;
+
+
+
+    TextView txtResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnReiniciar = findViewById(R.id.btnReiniciar);
+        txtResultado = findViewById(R.id.txtResultado);
+
+        btnReiniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reiniciarJogo();
+            }
+        });
     }
 
+    public void reiniciarJogo() {
+
+
+        txtResultado.setText("RESULTADO");
+
+        ImageView imageResultado = findViewById(R.id.imgApp);
+
+        imageResultado.setImageResource(R.drawable.padrao);
+    }
 
     public void selecionadoPedra(View view) {
         this.opcaoSelecionado("pedra");
     }
 
-
     public void selecionadoPapel(View view) {
         this.opcaoSelecionado("papel");
     }
-
 
     public void selecionadoTesoura(View view) {
         this.opcaoSelecionado("tesoura");
     }
 
-
     public void opcaoSelecionado(String opcaoSelecionada) {
-        ImageView imageResultado = findViewById(R.id.imgApp);
-        TextView txtResult = findViewById(R.id.txtResultado);
 
+        ImageView imageResultado = findViewById(R.id.imgApp);
 
         String opcoes[] = {"pedra", "papel", "tesoura"};
+
         String opcaoApp = opcoes[new Random().nextInt(3)];
+
         switch (opcaoApp) {
+
             case "pedra":
                 imageResultado.setImageResource(R.drawable.pedra);
                 break;
+
             case "papel":
                 imageResultado.setImageResource(R.drawable.papel);
                 break;
+
             case "tesoura":
                 imageResultado.setImageResource(R.drawable.tesoura);
                 break;
         }
+
         if ((opcaoApp.equals("tesoura") && opcaoSelecionada.equals("papel")) ||
                 (opcaoApp.equals("papel") && opcaoSelecionada.equals("pedra")) ||
                 (opcaoApp.equals("pedra") && opcaoSelecionada.equals("tesoura"))) {
-            txtResult.setText("Resultado: Você PERDEU... ");
+
+            txtResultado.setText("Resultado: Você PERDEU... ");
+
 
 
         } else if ((opcaoSelecionada.equals("tesoura") && opcaoApp.equals("papel")) ||
                 (opcaoSelecionada.equals("papel") && opcaoApp.equals("pedra")) ||
                 (opcaoSelecionada.equals("pedra") && opcaoApp.equals("tesoura"))) {
-            txtResult.setText("Resultado: Você GANHOU... ");
+
+            txtResultado.setText("Resultado: Você GANHOU... ");
+
+
+
         } else {
-            txtResult.setText("Resultado: Vocês EMPATARAM... ");
+
+            txtResultado.setText("Resultado: Vocês EMPATARAM... ");
         }
     }
 }
